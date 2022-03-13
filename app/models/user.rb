cf960fbf8bafaf9e117ec5e0999a07f40a6d7e4e
vaizu <vaizu07204869@yahoo.co.jp>
2022-03-13 03:22:12 +0000

    [Feature_Relationship]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,31 @@ class User < ApplicationRecord
 
   #プロフィールイメージ
   has_one_attached :user_image
+
   has_many :posts, dependent: :destroy
+  #いいね機能
   has_many :favorites, dependent: :destroy
+  #コメント機能
   has_many :comments, dependent: :destroy
+  #フォロー機能
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  #フォローする
+  def follow(user_id)
+    self.relationships.create!(followed_id: user_id)
+  end
+  #フォロー解除
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    self.followings.include?(user)
+  end
+
 
   def get_user_image
     unless user_image.attached?
