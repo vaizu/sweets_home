7f83c8ae537f040930d50681b51e800281e8f6e4
vaizu <vaizu07204869@yahoo.co.jp>
2022-03-14 07:44:34 +0000

    [Feature_Genre]

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
     resources :recipes, only: [:new,:show,:edit,:update,:create]
   end
+  
+  resources :genres, only: [:show]
 
 
 
