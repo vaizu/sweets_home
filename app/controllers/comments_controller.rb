a6d495d9323b7824d6a2bbb5ea29fe946198377a
vaizu <vaizu07204869@yahoo.co.jp>
2022-03-20 05:27:01 +0000

    [非同期通信]

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,16 +6,15 @@ class CommentsController < ApplicationController
     @comment_post = @comment.post
     if @comment.save
       @comment_post.create_notification_comment!(current_user, @comment.id)
-      redirect_to request.referrer
     else
-      redirect_to request.referrer
     end
   end
 
 
   def destroy
-    Comment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
+    @comment.destroy
   end
 
   private
